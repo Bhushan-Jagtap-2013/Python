aab66c0832c1968190b27ef994f78d5f18b97aa2
Bhushan <bhushan.jagtap.2013@gmail.com>
2017-08-06 17:54:24 -0400

    141. Linked List Cycle

---
+++ Link_List/SLL_loop.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/linked-list-cycle/description/
+# 141. Linked List Cycle : find loop
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head
+        slow = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
